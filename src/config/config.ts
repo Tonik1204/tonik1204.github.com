afd1e130fd0d62488b7593c2ba8e9b68bc0d435f
tonik1204 <las12041991@gmail.com>
2019-05-09 19:32:52 +0200

    build update 1.0

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,9 +5,9 @@ const MIN_DAY_HOURS = 6;
 const hours = new Date().getHours();
 
 const config = {
-  forecast_api_url: `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`,
+  forecast_api_url: `https://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`,
   city_api_url: `https://api.teleport.org/api/cities/?embed=city:search-results/city:item/{city:country,city:admin1_division,city:timezone/tz:offsets-now,city:urban_area}&limit=10&search=`,
-  forecast_icons_url: 'http://openweathermap.org/img/w/',
+  forecast_icons_url: 'https://openweathermap.org/img/w/',
 };
 
 export const toastTimer = 12000;
